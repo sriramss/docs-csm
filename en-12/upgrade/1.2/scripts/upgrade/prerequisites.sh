40dd5b72712ecf287e5748cecdf9fb35c044eada
Dennis Walker <denniswalker@me.com>
2022-03-23 06:17:19 -0700

    Revert "Generated HTML from docs-csm"
    This reverts commit 7f4f3a98834d86f55941ba0bc381a069ea065a8b.

--- en-12/upgrade/1.2/scripts/upgrade/prerequisites.sh
+++ en-12/upgrade/1.2/scripts/upgrade/prerequisites.sh
@@ -63,11 +63,6 @@ if [[ -z ${CSM_RELEASE} ]]; then
     exit 1
 fi
 
-if [[ -z ${SW_ADMIN_PASSWORD} ]]; then
-    echo "SW_ADMIN_PASSWORD environment variable has not been set"
-    exit 1
-fi
-
 if [[ -z ${TARBALL_FILE} ]]; then
     # Download tarball from internet
 
